cfe93ed99d44f0c444587b46fe095baa263a7fd6
matthias-blumenschein <matthiasblumenschein@gmail.com>
2022-12-06 19:16:32 +0100

    details page Grundgeruest erstellt

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -1,11 +1,19 @@
 import Header from "../components/Header/Header";
 import "./DetailsPage.css";
+import pikachu from "../img/pikachu.png";
 
 const DetailsPage = () => {
 	return (
 		<section className="details-page">
 			<Header />
-			<p>Details Page</p>
+
+			<div className="pokemon-img__container">
+				<div className="pokemon-img__box">
+					<img src={pikachu} alt="pokemon" className="pokemon-img__img" />
+				</div>
+			</div>
+
+			<h2>#004 Pikachu</h2>
 		</section>
 	);
 };
