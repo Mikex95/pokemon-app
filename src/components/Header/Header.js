b0408b86f987475e74ef24b4227b3f102371fd3b
matthias-blumenschein <matthiasblumenschein@gmail.com>
2022-12-06 14:23:57 +0100

    Grundgeruest erstelle

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import "./Header.css";
+
+const Header = () => {
+	return (
+		<header>
+			<p>Platzhalter für Logo</p>
+			<div>
+				<p>Burger-Menü</p>
+				<p>Suchleiste</p>
+				<p>Theme-Knopf</p>
+			</div>
+		</header>
+	);
+};
+
+export default Header;
---
+++ src/pages/DetailsPage.js
@@ -0,0 +1,11 @@
+import "./DetailsPage.css";
+
+const DetailsPage = () => {
+	return (
+		<section className="details-page">
+			<p>Details Page</p>
+		</section>
+	);
+};
+
+export default DetailsPage;
