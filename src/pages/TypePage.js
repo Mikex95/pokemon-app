7fb11d30c94a2ee9ab41328b6573f72795dfa0f8
Mikex95 <mike.beha1337@gmail.com>
2022-12-06 21:32:03 +0100

    re-edit typepage

--- src/pages/TypePage.js
+++ src/pages/TypePage.js
@@ -1,110 +1,123 @@
 import { Link } from "react-router-dom";
 import "./TypePage.css"
+import logo from '../img/pokemon-logo-small.png';
 
 
 const Typepage = () => {
     return ( <section className="typepage">
+        <section className="typepage-wrapper-headarea">
+            <div>
+                <img src={logo} alt="pokemon-logo" />
+            </div>
+            <div>
+            <Link to="/">
+                <button className="typepage-closing-button">
+                    X
+                </button>
+            </Link>
+            </div>
+        </section>
         <h1>TYPE</h1>
         <div className="typepage-grid">
         <Link to="/">
-            <button className="button-green">
+            <button className="button-green buttons">
                 BUG
             </button>
         </Link>
         <Link to="/">
-            <button className="button-dark">
+            <button className="button-dark buttons">
                 DARK
             </button>
         </Link>
         <Link to="/">
-            <button className="button-darkblue">
+            <button className="button-darkblue buttons">
                 DRAGON
             </button>
         </Link>
         <Link to="/">
-            <button className="button-yellow">
+            <button className="button-yellow buttons">
                 ELECTRIC
             </button>
         </Link>
         <Link to="/">
-            <button className="button-lightpink">
+            <button className="button-lightpink buttons">
                 FAIRY
             </button>
         </Link>
         <Link to="/">
-            <button className="button-red">
+            <button className="button-red buttons">
                 FIGHTING
             </button>
         </Link>
         <Link to="/">
-            <button className="button-orange">
+            <button className="button-orange buttons">
                 FIRE
             </button>
         </Link>
         <Link to="/">
-            <button className="button-lightgray">
+            <button className="button-lightgray buttons">
                 FLYING
             </button>
         </Link>
         <Link to="/">
-            <button className="button-purple">
+            <button className="button-purple buttons">
                 GHOST
             </button>
         </Link>
         <Link to="/">
-            <button className="button-darkgreen">
+            <button className="button-darkgreen buttons">
                 GRASS
             </button>
         </Link>
         <Link to="/">
-            <button className="button-brown">
+            <button className="button-brown buttons">
                 GROUND
             </button>
         </Link>
         <Link to="/">
-            <button className="button-lightblue">
+            <button className="button-lightblue buttons">
                 ICE
             </button>
         </Link>
         <Link to="/">
-            <button className="button-lightgray">
+            <button className="button-lightgray buttons">
                 NORMAL
             </button>
         </Link>
         <Link to="/">
-            <button className="button-lightgreen">
+            <button className="button-lightgreen buttons">
                 PLANT
             </button>
         </Link>
         <Link to="/">
-            <button className="button-lightpurple">
+            <button className="button-lightpurple buttons">
                 POISON
             </button>
         </Link>
         <Link to="/">
-            <button className="button-pink">
+            <button className="button-pink buttons">
                 PSYCHIC
             </button>
         </Link>
         <Link to="/">
-            <button className="button-darkyellow">
+            <button className="button-darkyellow buttons">
                 ROCK
             </button>
         </Link>
         <Link to="/">
-            <button className="button-darkblue">
+            <button className="button-darkblue buttons">
                 STEEL
             </button>
         </Link>
         <Link to="/">
-            <button className="button-blue">
+            <button className="button-blue buttons">
                 WATER
             </button>
         </Link>
         </div>
         <div className="typepage-search-button">
         <Link to="/">
-            <button className="button-yellow">
+            <button className="typepage-button-search">
                 SEARCH
             </button>
         </Link>
