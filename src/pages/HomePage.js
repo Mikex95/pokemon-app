131fa29c828a909cd3812d4d04d6b44da535cfc0
Mikex95 <mike.beha1337@gmail.com>
2022-12-06 22:32:31 +0100

    add-files-homePage.js-for-fetch

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,36 @@
+import {useState, useEffect} from "react"
 import Header from "../components/Header/Header";
 import GridItem from "../components/Pokemon-Overview/GridItem";
 import "./HomePage.css";
 
 const HomePage = () => {
+
+	const [pokemon, setPokemon] = useState([])
+	const [loading, setLoading] = useState(true)
+	const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
+	const [nextPageUrl, setNextPageUrl] = useState()
+	const [previousPageUrl, setPreviousPageUrl] = useState()
+
+	useEffect(() => {
+		setLoading(true)
+		fetch(currentPageUrl)
+    .then(response => response.json())
+    .then(data => {
+		setPokemon(data)
+		setNextPageUrl(data.next)
+		setPreviousPageUrl(data.previous)
+		setLoading(false)
+		
+	})
+  }, [currentPageUrl])
+  console.log(pokemon);
+  console.log(nextPageUrl);
+  console.log(previousPageUrl);
+	
+  if (loading) {
+	return "Waiting......"
+  }
+
 	return (
 		<section className="home-page">
 			<Header />
