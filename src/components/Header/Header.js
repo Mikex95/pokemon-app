f90025ba05d4b4cd7702c970c6666ca3f94212dc
“Qadaar-TU” <qadaar@gmail.com>
2022-12-07 14:59:05 +0100

    Burger menu fertig

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,26 +2,30 @@ import { Link } from "react-router-dom";
 import "./Header.css";
 import logo from "../../img/pokemon-logo-png-1421.png";
 import modus from "../../img/Vector.png";
+import filter from "./filter.png";
 
 const Header = () => {
-	return (
-		<header>
-			<div className="image-wrapper">
-				<img src={logo} alt="Logo" className="logo" />
-			</div>
-			<div className="flex-container">
-				<Link to={"/types"}>
-					<div className="menu-icon">
-						<span></span>
-						<span></span>
-						<span></span>
-					</div>
-				</Link>
-				<input type="text" name="" id="" placeholder="Search Pokemon" />
-				<img src={modus} alt="modus" className="modus-icon" />
-			</div>
-		</header>
-	);
+  return (
+    <header>
+      <div className="image-wrapper">
+        <img src={logo} alt="Logo" className="logo" />
+      </div>
+      <div className="flex-container">
+        <Link to={"/types"}>
+          <div id="webapp_cover">
+            <div id="menu_button">
+              <input type="checkbox" id="menu_checkbox" />
+              <label for="menu_checkbox" id="menu_label">
+                <div id="menu_text_bar"></div>
+              </label>
+            </div>
+          </div>
+        </Link>
+        <input type="text" name="" id="" placeholder="Search Pokemon" />
+        <img src={modus} alt="modus" className="modus-icon" />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
